f011415f80a456483f2d62c117b2817fb477080c
Remy <plespagnol@sfr.fr>
2013-11-23 14:47:10 +0100

    update

--- include/database.php
+++ include/database.php
@@ -1,7 +1,7 @@
 <?php
 try
 {
-$db = new PDO('mysql:host=localhost;dbname=training-day', 'root', '', $option);
+$db = new PDO('mysql:host=localhost;dbname=training-day', 'root', '');
 $db->exec("SET NAMES UTF8");
 }
 catch (Exception $e)
