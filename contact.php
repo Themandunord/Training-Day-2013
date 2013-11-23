808b4b1a9b60133fb7dc4b88d717172f6fa3754e
Gael Alberola <zoski@free.fr>
2013-11-23 15:27:00 +0100

    plop

---
+++ contact.php
@@ -0,0 +1,6 @@
+<?php include "include/header.php"; ?>
+	<div class="container">
+		<h1>Contact</h1>		
+		<p>Bobcat tiger so tom for singapura. Cheetah norwegian forest or lynx. Bombay tomcat for tiger for cheetah bobcat. Singapura tabby thai tabby havana brown lynx. Devonshire rex russian blue or cougar so scottish fold and tiger. Havana brown lynx russian blue and sphynx american shorthair, sphynx. Devonshire rex cougar cougar. Havana brown. Maine coon siberian siamese american bobtail tomcat birman. Savannah balinese . Tomcat puma. </p>
+	</div>
+<?php include "include/footer.php"; ?>
